144b1fda15dedb9898e1743bf186e9575f33026f
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2024-07-06 16:56:45 -0300

    Made the size of the array changeable and also some minor changes.

--- TheLargestAndTheSmallestValueInAArray.cpp
+++ TheLargestAndTheSmallestValueInAArray.cpp
@@ -4,19 +4,27 @@
 int main()
 {
 	using namespace std;
-	int Numbers[10]; // The array was set with ten variables but this code works with bigger arrays.
-	int Largest{ 0 };
+	int ArraySize; // This variable sets the maximum size of the array
+	int Numbers[20]; // The array was set with twenty variables but this code also works with bigger arrays, you just need to increase its size.
+	int Largest{ INT_MIN }; //  It's necessary to set the minimum value for this variable otherwise it wouldn't be replaced by the right value.
 	int Smallest{ INT_MAX }; // It's necessary to set the maximum value for this variable otherwise it wouldn't be replaced by the right value.
 
-	for (int i = 0; i < 10; i++)
+	// How I set the array size
+	cout << "Set the size of the array (Max. 20): ";
+	cin >> ArraySize;
+	system("CLS");
+
+	// Data input & processing
+	for (int i = 0; i < ArraySize; i++)
 	{
 		cout << "Type number " << (i + 1) << " : ";
-		cin >> Numbers[i];
-		if (Numbers[i] < Smallest)
+		cin >> Numbers[i]; // Data input
+		if (Numbers[i] < Smallest) // Processing of the smallest number in the array
 			Smallest = Numbers[i];
-		if (Numbers[i] > Largest)
+		if (Numbers[i] > Largest)  // Processing of the largest number in the array
 			Largest = Numbers[i];
 	}
 
+	// Data output (result)
 	cout << "The smallest number is " << Smallest << " and the largest is " << Largest;
 }
